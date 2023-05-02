6043fcc0c308cc16c8a5611f62c2715ae0100ca4
Eugene Korir <korir16071@gmail.com>
2023-05-02 17:06:35 +0300

    Fixed lifetimes and references issues

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,40 @@
+use std::vec;
+
 #[derive(Debug)]
 #[allow(unused)]
-struct Element<'a> {
+struct Element<'a>{
     value: String,
-    reference: Option<&'a Vec<&'a Element<'a>>>
+    reference: Option<Vec<&'a Element<'a>>>
     // reference: Option<Vec<&'a Element<'a>>>s
 }
 
-impl Element<'_> {
+impl<'a> Element<'a> {
     fn new(letter: &str) -> Self {
         Element { value: letter.to_string(), reference: None }
     }
-    fn new_ref<'a> (letter: &'a str, reference: &'a Vec<&'a Element<'a>>) -> &'a Self {
-        &Self { value: letter.to_string(), reference: Some(reference) }
+    fn new_ref (letter: &'a str, reference: Vec<&'a Element<'a>>) -> Self {
+        let ref _x = *reference; 
+        // &Element { value: letter.to_string(), reference: Some(x) }
+        Self { value: letter.to_string(), reference: Some(reference) }
+        // &Element { value: letter.to_string(), reference: Some( &vec![&Element]) }
     }
 }
 
 
 #[allow(unused)]
 fn main() {
-    let a = Element { value: "a".to_string(), reference: None };
-    let b = Element { value: "b".to_string(), reference: Some(&vec![&a]) };
+    let a = Element::new("a");
+    let b = Element::new_ref("b", vec![&a]);;
     let c = Element::new("c");
+    let d = Element::new_ref("d", vec![&b , &c]);
 
     
     let mut v = Vec::new();
 
     v.push(&a);
     v.push(&b);
+    v.push(&c);
+    v.push(&d);
 
     println!("{:#?}",v);
 
