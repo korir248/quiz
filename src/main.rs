bd2fb2555d231204cf54c532ac1aa1bd6b1c3fb6
Eugene Korir <korir16071@gmail.com>
2023-05-03 19:46:27 +0300

    Created insert_element function

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,81 @@
-use std::vec;
+enum ElemError {
+    InsertionError,
+    OtherError,
+}
 
 #[derive(Debug)]
 #[allow(unused)]
-struct Element<'a>{
+struct Element<'a> {
     value: String,
-    reference: Option<Vec<&'a Element<'a>>>
-    // reference: Option<Vec<&'a Element<'a>>>s
+    reference: Option<Vec<&'a Element<'a>>>,
 }
 
 impl<'a> Element<'a> {
     fn new(letter: &str) -> Self {
-        Element { value: letter.to_string(), reference: None }
+        Element {
+            value: letter.to_string(),
+            reference: None,
+        }
     }
-    fn new_ref (letter: &'a str, reference: Vec<&'a Element<'a>>) -> Self {
-        let ref _x = *reference; 
-        // &Element { value: letter.to_string(), reference: Some(x) }
-        Self { value: letter.to_string(), reference: Some(reference) }
-        // &Element { value: letter.to_string(), reference: Some( &vec![&Element]) }
+    fn new_ref(letter: &'a str, reference: Vec<&'a Element<'a>>) -> Self {
+        Self {
+            value: letter.to_string(),
+            reference: Some(reference),
+        }
     }
 }
 
+impl PartialEq for Element<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.value == other.value && self.reference == other.reference
+    }
+}
 
 #[allow(unused)]
 fn main() {
     let a = Element::new("a");
-    let b = Element::new_ref("b", vec![&a]);;
+    let b = Element::new_ref("b", vec![&a]);
     let c = Element::new("c");
-    let d = Element::new_ref("d", vec![&b , &c]);
+    let d = Element::new_ref("d", vec![&b]);
+    let e = Element::new_ref("e", vec![&a, &d]);
 
-    
-    let mut v = Vec::new();
+    let mut v: Vec<&Element> = Vec::new();
 
-    v.push(&a);
-    v.push(&b);
-    v.push(&c);
-    v.push(&d);
+    insert_element(&mut v, &a);
+    insert_element(&mut v, &a);
+    insert_element(&mut v, &b);
+    insert_element(&mut v, &e);
 
-    println!("{:#?}",v);
+    // v.push(&a);
+    // v.push(&b);
+    // v.push(&c);
+    // v.push(&d);
 
+    println!("{:#?}", v);
+}
+
+fn insert_element<'a>(
+    list: &mut Vec<&'a Element<'a>>,
+    element: &'a Element,
+) -> Result<(), ElemError> {
+    if list.contains(&element) {
+        println!("Element already in vector");
+        return Ok(());
+    }
+    if let Some(x) = &element.reference {
+        for i in x.iter() {
+            if !list.contains(i) {
+                println!("References a non existent element!");
+                return Err(ElemError::InsertionError);
+            } else {
+                list.push(element);
+                return Ok(());
+            }
+        }
+        // return Err(ElemError::InsertionError);
+    }
+
+    list.push(&element);
+
+    return Ok(());
 }
